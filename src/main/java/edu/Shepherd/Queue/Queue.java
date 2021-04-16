5d3dd61b2d44aca08f3597bbc8ce5f3c2735cf58
sgriff01@rams.shepherd.edu <stevengriffinjr@aol.com>
2021-04-16 02:23:30 -0400

    hopefully

---
+++ src/main/java/edu/Shepherd/Queue/Queue.java
@@ -0,0 +1,53 @@
+package edu.Shepherd.Queue;
+public class Queue<E> {
+    Node<E> head;   
+    public class Node<F>{
+        F data;
+        Node<F> link;
+        public Node(F data){
+            this.data = data;
+            link = null;
+            }
+        }  
+        public Queue(){
+            head=null;
+        }
+        public void insert(E data){
+            if(head==null){
+                head= new Node(data);
+            }  else {
+                Node<E> current =head;
+                while (current.link!=null){
+                    current = current.link;
+                }
+                current.link =new Node(data);
+            }
+        }
+        public E pop(){
+            Node<E> target = head;
+            head= head.link;
+            return target.data;
+        }
+
+        public int size(){
+            int size =0;
+            Node<E> current =head;
+            while (current!=null){
+                size++;
+                current= current.link;
+            }
+            return size;
+        }
+
+        public boolean isEmpty(){
+            if (head ==null){
+                return true;
+            }
+            return false; 
+        }
+
+        public E peek(){
+            return head.data;
+        }
+
+    }
